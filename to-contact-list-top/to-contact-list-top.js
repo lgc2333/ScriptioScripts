522a8076b2fa98002ddbc4e3f93d8834a809043c
student_2333 <lgc2333@126.com>
2024-01-31 20:47:38 +0800

    update

--- to-contact-list-top/to-contact-list-top.js
+++ to-contact-list-top/to-contact-list-top.js
@@ -1,5 +1,8 @@
-// * 添加一个回到聊天列表顶部的侧边栏按钮
+// * 添加一个回到聊天列表顶部的侧边栏按钮 - v1.1
 // @run-at main
+
+// 更新日志：
+// v1.1: 修复 #1、添加实时响应支持
 
 (function () {
   const topArrowSvg = `<svg
@@ -18,13 +21,19 @@
   const contactListSelector = '.q-scroll-view.recent-contact-list';
   const lowerSidebarSelector = '.sidebar__lower .sidebar__menu';
   const barItemSelector = `${lowerSidebarSelector} .func-menu__item_wrap`;
+  const toTopClassName = 'to-top-item';
+
+  /** @type {string} */
+  const selfPath = document.currentScript.getAttribute('data-scriptio-script');
+
+  let currentEnabled = false;
 
   /**
    * @param {string} iconHtml
    * @param {string} label
    * @returns {Node}
    */
-  function createLeftBarItem(iconHtml, label) {
+  function createLeftBarItem(iconHtml, label, className) {
     const templateLeftBarElem = document
       .querySelector(barItemSelector)
       .cloneNode(true);
@@ -32,7 +41,7 @@
     /** @type {HTMLDivElement} */
     const itemChildElem =
       templateLeftBarElem.firstElementChild.firstElementChild;
-    itemChildElem.classList.value = 'to-top-item func-menu__item vue-component';
+    itemChildElem.classList.value = `${className} func-menu__item vue-component`;
     templateLeftBarElem
       .querySelector('div[aria-label]')
       .setAttribute('aria-label', label);
@@ -44,13 +53,36 @@
     contactListElem.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
-  function main() {
-    const toTopElem = createLeftBarItem(topArrowSvg, '回顶部');
-    toTopElem.addEventListener('click', toContactListTop);
-    const sidebarLowerElem = document.querySelector(lowerSidebarSelector);
-    sidebarLowerElem.insertBefore(toTopElem, sidebarLowerElem.firstChild);
+  const toTopElem = createLeftBarItem(topArrowSvg, '回顶部', toTopClassName);
+  toTopElem.addEventListener('click', toContactListTop);
+  const sidebarLowerElem = document.querySelector(lowerSidebarSelector);
+
+  /**
+   * @param {boolean} state
+   */
+  function toggle(state) {
+    const enable = () => {
+      sidebarLowerElem.insertBefore(toTopElem, sidebarLowerElem.firstChild);
+    };
+
+    const disable = () => {
+      toTopElem.remove();
+    };
+
+    if (state && !currentEnabled) enable();
+    else if (!state && currentEnabled) disable();
+    currentEnabled = state;
   }
 
+  /**
+   * @param {CustomEvent<{path: string, enabled: bool}>} event
+   */
+  const toggleListener = (event) => {
+    const { path, enabled } = event.detail;
+    console.log(event.detail, selfPath);
+    if (path === selfPath) toggle(enabled);
+  };
+  window.addEventListener('scriptio-toggle', toggleListener);
+  toggle(true);
   globalThis.toContactListTop = toContactListTop;
-  setTimeout(main);
 })();
